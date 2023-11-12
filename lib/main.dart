bc275bdc1a99695d504bf50770a9c7023480b7c7
CK Phani Datta <phanidatta673@gmail.com>
2023-11-12 16:04:44 -0600

    login page and firebase auth modifications

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,24 @@
 import 'package:flutter/material.dart';
-import 'package:tripool_app/screens/bottom_bar_screen.dart';
-import 'package:tripool_app/screens/login.dart';
+// import 'package:tripool_app/screens/login.dart';
+import 'package:tripool_app/screens/login_page.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:tripool_app/screens/main_page.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
-void main() {
+void main() async {
+  // await dotenv.load();
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp(
+    options: FirebaseOptions(
+      apiKey: dotenv.env['API_KEY']??'',
+      projectId: dotenv.env['PROJECT_ID']??'',
+      storageBucket: dotenv.env['STORAGE_BUCKET']??'',
+      messagingSenderId: dotenv.env['MESSAGING_SENDER_ID']??'',
+      appId: dotenv.env['APP_ID']??'',
+      
+    ),
+  );
   runApp(const MyApp());
 }
 
@@ -22,7 +38,7 @@ class MyApp extends StatelessWidget {
             iconTheme: IconTheme.of(context).copyWith(
               color: Colors.white,
             )),
-        home: const Login(),
+        home: const MainPage(),
       );
   }
 }
