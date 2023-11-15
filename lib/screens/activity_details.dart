490548f25ee5d8356babd931c67a34309d4ba31d
AndrewLester <alester220@gmail.com>
2023-11-14 20:25:38 -0600

    Some progress

--- lib/screens/activity_details.dart
+++ lib/screens/activity_details.dart
@@ -50,126 +50,137 @@ class _DetailsPageState extends State<DetailsPage> {
           print(To_Date);
           print(To_Time);
 
+          var actionButtons = [OutlineButton(
+            child: Text('Request Group'),
+            onPressed: () async {
+              final userDoc = FirebaseFirestore.instance.collection('Users').doc(FirebaseAuth.instance.currentUser.id);
+              final user = await userDoc.get();
+              await userDoc.update({'Requested_Activities': [...user.get('Requested_Activities'), widget.activityId]});
+            },
+          )]
+
+  // [
+  //                       ElevatedButton(
+  //                         child: Text('Join Now'),
+  //                         onPressed: isCountEqual
+  //                             ? () async {
+  //                                 _launchURL(snapshotDoc.get('sigLink'));
+  //                               }
+  //                             : null,
+  //                       ),
+  //                       ElevatedButton(
+  //                         child: Text('Interested'),
+  //                         onPressed: () async {
+  //                           await FirebaseFirestore.instance
+  //                               .collection('sigs')
+  //                               .doc(widget.sigId)
+  //                               .update({
+  //                             'interestedCount':
+  //                                 snapshotDoc.get('interestedCount') + 1
+  //                           });
+
+  //                           await FirebaseFirestore.instance
+  //                               .collection('users')
+  //                               .doc(FirebaseAuth.instance.currentUser!.uid)
+  //                               .collection('interestedSigs')
+  //                               .doc(widget.sigId)
+  //                               .set({
+  //                             'sigId': widget.sigId,
+  //                             'sigTitle': snapshotDoc.get('sigTitle'),
+  //                           });
+
+  //                           print(snapshotDoc.get('interestedCount'));
+  //                           print(snapshotDoc.get('sigCount'));
+  //                           if (snapshotDoc.get('interestedCount') ==
+  //                               snapshotDoc.get('sigCount')) {
+  //                             print('this is working');
+  //                             await FirebaseFirestore.instance
+  //                                 .collection('sigs')
+  //                                 .doc(widget.sigId)
+  //                                 .update({'isConfirmed': true});
+  //                           }
+  //                         },
+  //                       ),
+  //                     ],
+
           return Container(
-              // child: Padding(
-              //   padding: const EdgeInsets.fromLTRB(20, 30, 20, 0),
-              //   child: Column(
-              //     crossAxisAlignment: CrossAxisAlignment.start,
-              //     children: [
-              //       Text(
-              //         'Title : ' + snapshotDoc.get('sigTitle'),
-              //         style: TextStyle(fontSize: 16),
-              //       ),
-              //       SizedBox(height: 20),
-              //       Divider(thickness: 1.5),
-              //       SizedBox(height: 20),
-              //       Text(
-              //         'Description: ' + snapshotDoc.get('sigDesc'),
-              //         style: TextStyle(fontSize: 16),
-              //       ),
-              //       SizedBox(height: 20),
-              //       Divider(thickness: 1.5),
-              //       SizedBox(height: 20),
-              //       Text(
-              //         'Conducted By: ' + snapshotDoc.get('sigByName'),
-              //         style: TextStyle(fontSize: 16),
-              //       ),
-              //       SizedBox(height: 15),
-              //       Text(
-              //         'Proficiency of Host : ' + snapshotDoc.get('proficiency'),
-              //         style: TextStyle(fontSize: 16),
-              //       ),
-              //       SizedBox(height: 20),
-              //       Divider(thickness: 1.5),
-              //       SizedBox(height: 20),
-              //       Row(
-              //         mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              //         children: [
-              //           Text("Date of SIG"),
-              //           Text(DateFormat.jms().format(
-              //               DateTime.fromMillisecondsSinceEpoch(
-              //                   snapshotDoc.get('sigDateTime').seconds * 1000))),
-              //         ],
-              //       ),
-              //       SizedBox(height: 15),
-              //       Row(
-              //         mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              //         children: [
-              //           Text('Time of SIG'),
-              //           Text(DateFormat.yMMMEd()
-              //               .format(DateTime.fromMillisecondsSinceEpoch(
-              //                   snapshotDoc.get('sigDateTime').seconds * 1000))
-              //               .toString()),
-              //         ],
-              //       ),
-              //       SizedBox(height: 20),
-              //       Divider(thickness: 1.5),
-              //       SizedBox(height: 20),
-              //       Text('Topics : '),
-              //       Text(
-              //         snapshotDoc.get('topics'),
-              //         style: TextStyle(fontSize: 16),
-              //       ),
-              //       SizedBox(height: 20),
-              //       Divider(thickness: 1.5),
-              //       SizedBox(height: 20),
-              //       Text('Registered Enthusiasts Count : ' +
-              //           snapshotDoc.get('interestedCount').toString()),
-              //       Text('Join Now would be activated after : ' +
-              //           (snapshotDoc.get('sigCount') -
-              //                   snapshotDoc.get('interestedCount'))
-              //               .toString() +
-              //           ' entries'),
-              //       SizedBox(height: 40),
-              //       Row(
-              //         mainAxisAlignment: MainAxisAlignment.spaceAround,
-              //         children: [
-              //           ElevatedButton(
-              //             child: Text('Join Now'),
-              //             onPressed: isCountEqual
-              //                 ? () async {
-              //                     _launchURL(snapshotDoc.get('sigLink'));
-              //                   }
-              //                 : null,
-              //           ),
-              //           ElevatedButton(
-              //             child: Text('Interested'),
-              //             onPressed: () async {
-              //               await FirebaseFirestore.instance
-              //                   .collection('sigs')
-              //                   .doc(widget.sigId)
-              //                   .update({
-              //                 'interestedCount':
-              //                     snapshotDoc.get('interestedCount') + 1
-              //               });
-
-              //               await FirebaseFirestore.instance
-              //                   .collection('users')
-              //                   .doc(FirebaseAuth.instance.currentUser!.uid)
-              //                   .collection('interestedSigs')
-              //                   .doc(widget.sigId)
-              //                   .set({
-              //                 'sigId': widget.sigId,
-              //                 'sigTitle': snapshotDoc.get('sigTitle'),
-              //               });
-
-              //               print(snapshotDoc.get('interestedCount'));
-              //               print(snapshotDoc.get('sigCount'));
-              //               if (snapshotDoc.get('interestedCount') ==
-              //                   snapshotDoc.get('sigCount')) {
-              //                 print('this is working');
-              //                 await FirebaseFirestore.instance
-              //                     .collection('sigs')
-              //                     .doc(widget.sigId)
-              //                     .update({'isConfirmed': true});
-              //               }
-              //             },
-              //           ),
-              //         ],
-              //       )
-              //     ],
-              //   ),
-              // ),
+              child: Padding(
+                padding: const EdgeInsets.fromLTRB(20, 30, 20, 0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      'Title : ' + snapshotDoc.get('sigTitle'),
+                      style: TextStyle(fontSize: 16),
+                    ),
+                    SizedBox(height: 20),
+                    Divider(thickness: 1.5),
+                    SizedBox(height: 20),
+                    Text(
+                      'Description: ' + snapshotDoc.get('sigDesc'),
+                      style: TextStyle(fontSize: 16),
+                    ),
+                    SizedBox(height: 20),
+                    Divider(thickness: 1.5),
+                    SizedBox(height: 20),
+                    Text(
+                      'Conducted By: ' + snapshotDoc.get('sigByName'),
+                      style: TextStyle(fontSize: 16),
+                    ),
+                    SizedBox(height: 15),
+                    Text(
+                      'Proficiency of Host : ' + snapshotDoc.get('proficiency'),
+                      style: TextStyle(fontSize: 16),
+                    ),
+                    SizedBox(height: 20),
+                    Divider(thickness: 1.5),
+                    SizedBox(height: 20),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Text("Date of SIG"),
+                        Text(DateFormat.jms().format(
+                            DateTime.fromMillisecondsSinceEpoch(
+                                snapshotDoc.get('sigDateTime').seconds * 1000))),
+                      ],
+                    ),
+                    SizedBox(height: 15),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Text('Time of SIG'),
+                        Text(DateFormat.yMMMEd()
+                            .format(DateTime.fromMillisecondsSinceEpoch(
+                                snapshotDoc.get('sigDateTime').seconds * 1000))
+                            .toString()),
+                      ],
+                    ),
+                    SizedBox(height: 20),
+                    Divider(thickness: 1.5),
+                    SizedBox(height: 20),
+                    Text('Topics : '),
+                    Text(
+                      snapshotDoc.get('topics'),
+                      style: TextStyle(fontSize: 16),
+                    ),
+                    SizedBox(height: 20),
+                    Divider(thickness: 1.5),
+                    SizedBox(height: 20),
+                    Text('Registered Enthusiasts Count : ' +
+                        snapshotDoc.get('interestedCount').toString()),
+                    Text('Join Now would be activated after : ' +
+                        (snapshotDoc.get('sigCount') -
+                                snapshotDoc.get('interestedCount'))
+                            .toString() +
+                        ' entries'),
+                    SizedBox(height: 40),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceAround,
+                      children: actionButtons,
+                    )
+                  ],
+                ),
+              ),
               );
         },
       ),
