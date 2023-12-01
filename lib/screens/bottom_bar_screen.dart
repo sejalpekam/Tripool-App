d56bf930132294b763f8af33e8e1fa8dedfa163c
luozx <luozx@umich.edu>
2023-12-01 01:45:30 -0600

    merge notif to main

--- lib/screens/bottom_bar_screen.dart
+++ lib/screens/bottom_bar_screen.dart
@@ -1,3 +1,5 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:persistent_bottom_nav_bar/persistent_tab_view.dart';
 import 'package:tripool_app/screens/tabs/create_tab.dart';
@@ -26,44 +28,70 @@ class _BottomBarScreenState extends State<BottomBarScreen> {
   List<Widget> _buildTabs() {
     return [
       ScheduleTab(),
-      // HomeTab(),
       CreateActivityTab(),
       ExploreTab(),
       ProfileTab(),
     ];
   }
 
-  List<PersistentBottomNavBarItem> _navBarItems() {
+  Stream<bool> hasNotifications() {
+    final currentUser = FirebaseAuth.instance.currentUser;
+    return FirebaseFirestore.instance
+      .collection('Activity')
+      .snapshots()
+      .map((snapshot) => snapshot.docs.any((doc) {
+        final data = doc.data();
+        final notifRequest = data['Notif_Request'] as List<dynamic>? ?? [];
+        final notifLeftActivity = data['Notif_LeftActivity'] as List<dynamic>? ?? [];
+        final notifAcceptedRequest = data['Notif_AcceptedRequest'] as List<dynamic>? ?? [];
+        final notifRemoveMembers = data['Notif_RemovedMembers'] as List<dynamic>? ?? [];
+        bool isCurrentUserInvolved = notifAcceptedRequest.contains(currentUser?.uid) || notifRemoveMembers.contains(currentUser?.uid);
+        return isCurrentUserInvolved || (currentUser?.uid == data['Creator'] && (notifRequest.isNotEmpty || notifLeftActivity.isNotEmpty));
+      }));
+  }
+
+  List<PersistentBottomNavBarItem> _navBarItems(bool showIndicator) {
     return [
       PersistentBottomNavBarItem(
-        icon: const Icon(Icons.calendar_month),
+        icon: showIndicator
+            ? Stack(
+                children: [
+                  const Icon(Icons.calendar_month),
+                  Positioned(
+                    right: 0,
+                    top: 0,
+                    child: Container(
+                      padding: const EdgeInsets.all(2),
+                      decoration: const BoxDecoration(
+                        color: Colors.red,
+                        shape: BoxShape.circle,
+                      ),
+                      constraints: const BoxConstraints(
+                        minWidth: 8,
+                        minHeight: 8,
+                      ),
+                    ),
+                  ),
+                ],
+              )
+            : const Icon(Icons.calendar_month),
         title: ("Schedule"),
         activeColorPrimary: Theme.of(context).colorScheme.primary,
-        // inactiveColorPrimary: Colors.grey,
       ),
-      // PersistentBottomNavBarItem(
-      //   icon: const Icon(Icons.home),
-      //   title: ("Home"),
-      //   activeColorPrimary: Theme.of(context).colorScheme.primary,
-      //   // inactiveColorPrimary: Colors.grey,
-      // ),
       PersistentBottomNavBarItem(
         icon: const Icon(Icons.create_new_folder_rounded),
         title: ("Create"),
         activeColorPrimary: Theme.of(context).colorScheme.primary,
-        // inactiveColorPrimary: Colors.grey,
       ),
       PersistentBottomNavBarItem(
         icon: const Icon(Icons.explore),
         title: ("Explore"),
         activeColorPrimary: Theme.of(context).colorScheme.primary,
-        // inactiveColorPrimary: Colors.grey,
       ),
       PersistentBottomNavBarItem(
         icon: const Icon(Icons.person),
         title: ("Profile"),
         activeColorPrimary: Theme.of(context).colorScheme.primary,
-        // inactiveColorPrimary: Colors.grey,
       ),
     ];
   }
@@ -72,37 +100,38 @@ class _BottomBarScreenState extends State<BottomBarScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(),
-      bottomNavigationBar: PersistentTabView(
-        context,
-        controller: _controller,
-        screens: _buildTabs(),
-        items: _navBarItems(),
-        confineInSafeArea: true,
-        handleAndroidBackButtonPress: true, // Default is true.
-        resizeToAvoidBottomInset:
-            true, // This needs to be true if you want to move up the screen when keyboard appears. Default is true.
-        stateManagement: true, // Default is true.
-        hideNavigationBarWhenKeyboardShows:
-            true, // Recommended to set 'resizeToAvoidBottomInset' as true while using this argument. Default is true.
-        decoration: NavBarDecoration(
-          borderRadius: BorderRadius.circular(10.0),
-          colorBehindNavBar: Colors.white,
-        ),
-        popAllScreensOnTapOfSelectedTab: true,
-        popActionScreens: PopActionScreensType.all,
-        itemAnimationProperties: const ItemAnimationProperties(
-          // Navigation Bar's items animation properties.
-          duration: Duration(milliseconds: 200),
-          curve: Curves.ease,
-        ),
-        screenTransitionAnimation: const ScreenTransitionAnimation(
-          // Screen transition animation on change of selected tab.
-          animateTabTransition: true,
-          curve: Curves.ease,
-          duration: Duration(milliseconds: 200),
-        ),
-        navBarStyle:
-            NavBarStyle.style1, // Choose the nav bar style with this property.
+      bottomNavigationBar: StreamBuilder<bool>(
+        stream: hasNotifications(),
+        builder: (context, snapshot) {
+          return PersistentTabView(
+            context,
+            controller: _controller,
+            screens: _buildTabs(),
+            items: _navBarItems(snapshot.data ?? false),
+            confineInSafeArea: true,
+            // ... other properties
+            handleAndroidBackButtonPress: true,
+            resizeToAvoidBottomInset: true,
+            stateManagement: true,
+            hideNavigationBarWhenKeyboardShows: true,
+            decoration: NavBarDecoration(
+              borderRadius: BorderRadius.circular(10.0),
+              colorBehindNavBar: Colors.white,
+            ),
+            popAllScreensOnTapOfSelectedTab: true,
+            popActionScreens: PopActionScreensType.all,
+            itemAnimationProperties: const ItemAnimationProperties(
+              duration: Duration(milliseconds: 200),
+              curve: Curves.ease,
+            ),
+            screenTransitionAnimation: const ScreenTransitionAnimation(
+              animateTabTransition: true,
+              curve: Curves.ease,
+              duration: Duration(milliseconds: 200),
+            ),
+            navBarStyle: NavBarStyle.style1,
+          );
+        }
       ),
     );
   }
--- lib/widgets/event_widget.dart
+++ lib/widgets/event_widget.dart
@@ -5,97 +5,106 @@ import '../../model/event.dart';
 
 class EventWidget extends StatelessWidget {
   final Event event;
+  final bool hasNotifications; // Include the hasNotifications field
 
-  const EventWidget({Key? key, required this.event}) : super(key: key);
+  // Add hasNotifications to the constructor with a default value of false
+  const EventWidget({Key? key, required this.event, this.hasNotifications = false}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Card(
-      margin: const EdgeInsets.symmetric(vertical: 20),
-      elevation: 4,
-      color: Colors.white,
-      shape: RoundedRectangleBorder(borderRadius: BorderRadius.all(Radius.circular(24))),
-      child: Padding(
-        padding: const EdgeInsets.all(20),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: <Widget>[
-            Padding(
-              padding: const EdgeInsets.only(top: 8.0, left: 8.0),
-              child: Row(
-                children: <Widget>[
-                  Expanded(
-                    flex: 3,
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: <Widget>[
-                        Text(
-                          event.title,
-                          style: eventTitleTextStyle,
-                        ),
-                        SizedBox(
-                          height: 10,
-                        ),
-                        FittedBox(
-                          child: Row(
-                            children: <Widget>[
-                              Icon(Icons.location_on),
-                              SizedBox(
-                                width: 5,
+    return Stack(
+      children: [
+        Card(
+          margin: const EdgeInsets.symmetric(vertical: 20),
+          elevation: 4,
+          color: Colors.white,
+          shape: RoundedRectangleBorder(borderRadius: BorderRadius.all(Radius.circular(24))),
+          child: Padding(
+            padding: const EdgeInsets.all(20),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: <Widget>[
+                Padding(
+                  padding: const EdgeInsets.only(top: 8.0, left: 8.0),
+                  child: Row(
+                    children: <Widget>[
+                      Expanded(
+                        flex: 3,
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: <Widget>[
+                            Text(
+                              event.title,
+                              style: eventTitleTextStyle,
+                            ),
+                            SizedBox(height: 10),
+                            FittedBox(
+                              child: Row(
+                                children: <Widget>[
+                                  Icon(Icons.location_on),
+                                  SizedBox(width: 5),
+                                  Text(
+                                    event.location,
+                                    style: eventLocationTextStyle,
+                                  ),
+                                ],
                               ),
-                              Text(
-                                event.location,
-                                style: eventLocationTextStyle,
+                            ),
+                            SizedBox(height: 10),
+                            FittedBox(
+                              child: Row(
+                                children: <Widget>[
+                                  Icon(Icons.person),
+                                  SizedBox(width: 5),
+                                  Text(
+                                    event.hostname,
+                                    style: eventLocationTextStyle.copyWith(fontSize: 15.0),
+                                  ),
+                                ],
                               ),
-                            ],
-                          ),
+                            ),
+                          ],
                         ),
-                        SizedBox(
-                          height: 10,
+                      ),
+                      Expanded(
+                        flex: 2,
+                        child: Column(
+                          children: [
+                            Text(
+                              event.startdate,
+                              textAlign: TextAlign.right,
+                              style: eventLocationTextStyle.copyWith(fontWeight: FontWeight.w700),
+                            ),
+                            SizedBox(height: 10),
+                            Text(
+                              event.starttime,
+                              textAlign: TextAlign.right,
+                              style: eventLocationTextStyle.copyWith(fontWeight: FontWeight.w700),
+                            ),
+                          ],
                         ),
-                        FittedBox(
-                          child: Row(
-                            children: <Widget>[
-                              Icon(Icons.person),
-                              SizedBox(
-                                width: 5,
-                              ),
-                              Text(
-                                event.hostname,
-                                style: eventLocationTextStyle.copyWith(fontSize: 15.0),
-                              ),
-                            ],
-                          ),
-                        ),
-                      ],
-                    ),
+                      ),
+                    ],
                   ),
-                  Expanded(
-                    flex: 2,
-                    child: Column(
-                      children: [
-                        Text(
-                          event.startdate,
-                          textAlign: TextAlign.right,
-                          style: eventLocationTextStyle.copyWith(fontWeight: FontWeight.w700),
-                          
-                        ),
-                        SizedBox(height: 10,),
-                        Text(
-                          event.starttime,
-                          textAlign: TextAlign.right,
-                          style: eventLocationTextStyle.copyWith(fontWeight: FontWeight.w700),
-                        ),
-                      ],
-                    ),
-                  ),
-                ],
-              ),
-              
+                ),
+              ],
             ),
-          ],
+          ),
         ),
-      ),
+        // Include the red dot if there are notifications
+        if (hasNotifications)
+          Positioned(
+            right: 5,
+            top: 20,
+            child: Container(
+              padding: EdgeInsets.all(8),
+              decoration: BoxDecoration(
+                color: Colors.red,
+                shape: BoxShape.circle,
+              ),
+            ),
+          ),
+      ],
     );
   }
 }
